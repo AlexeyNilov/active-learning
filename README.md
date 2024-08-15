15eeade49bdaa79c72d000c7810e5d1150c86885
alexey.nilov <alexey.nilov@ringcentral.com>
2024-08-15 14:29:22 +0200

    spiking

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 1. Choose a movie, get subtitles
 2. Get new verbs from the subtitles
 3. Create duocards
-4. Get related phrases/sentences
+4. Get related phrases/sentences?
 5. Practice
+6. Watch the movie
 
--- parsers/get_new_verbs.py
+++ parsers/get_new_verbs.py
@@ -2,7 +2,7 @@ from collections import Counter
 import spacy
 from data.words import get_known_verbs, get_ignore_list
 from data.srt import get_text_from_srt
-from conf.default import MIN_VERB_LEN, MIN_VERB_FREQUENCY
+from conf.default import MIN_VERB_LEN, MIN_VERB_FREQUENCY, MAX_SENTENCE_LENGTH
 
 # Load srt content
 file_path = 'srt/Iron.Man[2008]DvDrip-aXXo.srt'
@@ -27,3 +27,25 @@ ignore_words = get_ignore_list()
 filtered_verbs = [verb for verb in common_verbs_extracted if verb not in known_verbs + ignore_words]
 
 print(filtered_verbs)
+exit()
+
+
+# Function to extract sentences containing specific verbs
+def extract_sentences_with_verbs(doc, verbs):
+    sentences_with_verbs = []
+    for sent in doc.sents:
+        if len(sent) > MAX_SENTENCE_LENGTH:
+            continue
+
+        # Check if any of the common verbs appear in the sentence
+        if any(verb in [token.lemma_ for token in sent if token.pos_ == 'VERB'] for verb in verbs):
+            sentences_with_verbs.append(sent.text)
+    return sentences_with_verbs
+
+
+# Extract sentences containing the common verbs
+sentences_with_common_verbs = extract_sentences_with_verbs(doc, filtered_verbs)
+
+print("Sentences with Common Verbs:")
+for sentence in sentences_with_common_verbs[:10]:
+    print(sentence)
